2f962134534198708973e5125463d22475fa8e5f
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2020-05-10 17:35:25 +0200

    Generate devise User model

--- lib/ghost_rails/config/routes.rb
+++ lib/ghost_rails/config/routes.rb
@@ -1,3 +1,6 @@
-# Provide the absolute path to your application's config folder as an ENV var.
+Rails.application.routes.draw do
+  devise_for :users
+end
 
+# Provide the absolute path to your application's config folder as an ENV var.
 require File.join(ENV["CONFIG_DIR"], "routes.rb") if ENV["CONFIG_DIR"].present?
