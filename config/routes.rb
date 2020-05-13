3356b2c28cbb481b9440c9f32ab74fdccec14a7c
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2020-05-13 23:10:07 +0200

    Move Rails folder up
    No idea if that's such a good idea.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # root 'application#welcome'
   devise_for :users
   get "/account", to: "application#account"
 end
+
 
 # Provide the absolute path to your application's config folder as an ENV var.
 require File.join(ENV["CONFIG_DIR"], "routes.rb") if ENV["CONFIG_DIR"].present?
