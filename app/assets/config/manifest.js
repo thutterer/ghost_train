f13484af947b94aea50112e40cb1c102607af208
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2020-05-30 20:01:19 +0200

    Add Open Graph and Twitter meta tags

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,8 +4,8 @@
 //= link administrate/application.css
 //= link administrate/application.js
 
-//= link front/logo.svg
 //= link front/logo_192.png
 //= link front/logo_512.png
+//= link front/logo_large.png
 
 //= link_tree ../images
