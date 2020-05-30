ae0087156d1bfb45efa5885444636b0202a26ded
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2020-05-30 15:49:15 +0200

    Disable registration and changing email
    For now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         # :registerable,
+         # :recoverable,
+         :rememberable,
+         :validatable
 
   def admin?
     id == 1
