00569a14469c6de479ce9708030099ed6c11bb18
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2020-06-06 01:10:49 +0200

    Use the progressive start url as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root "front/application#welcome"
-
   get "manifest.json" => "progressive#manifest"
   get "offline" => "progressive#offline"
   get "service-worker.js" => "progressive#service_worker"
   get "start" => "progressive#start"
+  root "progressive#start"
 
   devise_for :users
 
