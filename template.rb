72434e0eefd5ddb9618e473043670e4f5ef3087a
Thomas Hutterer <thutterer@suse.de>
2021-04-28 19:19:51 +0200

    Improve engine template

--- template.rb
+++ template.rb
@@ -5,7 +5,11 @@ def add_config_method
   content = <<-RUBY
   def self.config
     {
-      theme_color: "#A63446"
+      name: "My Awesome Application",
+      short_name: "My app",
+      description: "Young and wild.",
+      theme_color: "#8338ec",
+      background_color: "#FAFAFA"
     }
   end
     RUBY
@@ -14,8 +18,18 @@ end
 
 def add_welcome
   route 'get "home" => "application#welcome"'
-  insert_into_file 'app/controllers/front/application_controller.rb', 'def welcome; end', after: "class ApplicationController < ActionController::Base\n"
-  create_file 'app/views/front/application/welcome.html.erb', 'Hello there!'
+    
+  insert_into_file 'app/controllers/front/application_controller.rb', "layout \"application\"\n\ndef welcome; end", after: "class ApplicationController < ActionController::Base\n"
+
+  welcome = <<-HTML
+<header>
+  <h1>Welcome aboard!</h1>
+  <%- if User.count.zero? %>
+    <p> Create your first user in the console. The first user will be admin.</p>
+  <% end %>
+</header>
+  HTML
+  create_file 'app/views/front/application/welcome.html.erb', welcome
 end
 
 def add_logos
@@ -28,8 +42,18 @@ def fix_javascript
   create_file "app/assets/javascripts/front/application.js"
 end
 
+def add_helpers
+  engine_name = <<-RUBY
+    def engine_name
+      "front"
+    end
+  RUBY
+  
+  insert_into_file 'app/helpers/front/application_helper.rb', engine_name, after: "module ApplicationHelper\n"
+end
 # Main setup
 add_welcome
 add_logos
 fix_javascript
 add_config_method
+add_helpers
