77472b605d08aa16e3a37994b1212b7a9b5bf2fa
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2021-05-02 11:31:26 +0200

    Save thing type on create

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,8 +1,7 @@
 class Thing < ApplicationRecord
   belongs_to :user, optional: true
-  store_accessor :data, :type
 
-  before_save do
-    type = model_name.singular_route_key
+  before_create do
+    data['type'] = model_name.singular_route_key
   end
 end
