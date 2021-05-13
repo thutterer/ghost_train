20712faf78a60ee02be7e48dae0012a82f4dee82
Thomas Hutterer <thutterer@suse.de>
2021-05-13 12:20:27 +0200

    Fix missing default_url_options for mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,7 @@ Rails.application.configure do
     config.action_mailer.delivery_method = :smtp
   end
 
-  # TODO: Devise needs this!
-  # config.action_mailer.default_url_options = { host: 'host', port: 555 }
+  config.action_mailer.default_url_options = { host: Front.config[:domain] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
