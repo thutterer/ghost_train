f92d104084e8b929d208fa8f098d63e78f51cfe5
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2021-05-27 20:59:26 +0200

    Remove explicit route for PWA start
    Instead use the root url, which renders the same action and is already in SW offline cache.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "manifest.json" => "progressive#manifest"
   get "offline" => "progressive#offline"
   get "service-worker.js" => "progressive#service_worker"
-  get "start" => "progressive#start"
   root "progressive#start"
 
   devise_for :users, controllers: {
