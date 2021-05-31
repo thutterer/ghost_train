df7531c9d954e6188cc17208e3439acbdc28aebb
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2021-05-31 13:09:21 +0200

    Fix dialog polyfill after Turbo visits

--- app/assets/javascripts/polyfills.js
+++ app/assets/javascripts/polyfills.js
@@ -1,4 +1,15 @@
 //= require polyfills/dialog-polyfill
 
-var dialog = document.querySelector('dialog')
-dialogPolyfill.registerDialog(dialog)
+function installPolyfills () {
+  // Set global `dialog` variable for easy access in SJR
+  dialog = document.querySelector('dialog')
+  dialogPolyfill.registerDialog(dialog)
+}
+
+// On first page load
+installPolyfills()
+
+// On every Turbo visit
+document.addEventListener("turbo:load", function() {
+  installPolyfills()
+})
